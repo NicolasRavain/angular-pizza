828301b239e6b2e9103b0518fa6f56a4fbf6e6c5
Nicolas Ravain <nicolasravain59@gmail.com>
2020-01-16 15:05:54 +0100

    menu collapse show hide

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent {
+	isCollapsed: boolean = false;
+	@Input() title: string;
+	@Input() color: string = 'light';
+
+	toggleCollapse() {
+		this.isCollapsed = !this.isCollapsed;
+	}
+
+switchTheme() {
+	this.color = ('light' === this.color) ? 'dark' : 'light';
+	}
+}
